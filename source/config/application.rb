57c6d0dea6be664c2d81dd5a6c8f98891ee68906
thienhv <thien.hoviet@gmail.com>
2015-10-28 17:37:35 +0700

    change views faqs

--- source/config/application.rb
+++ source/config/application.rb
@@ -27,6 +27,6 @@ module Ciaobox
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join("metronic")
-    config.assets.precompile += %w( *.css *.js )
+    config.assets.precompile += %w( *.css *.js *.png *.jpg)
   end
 end
