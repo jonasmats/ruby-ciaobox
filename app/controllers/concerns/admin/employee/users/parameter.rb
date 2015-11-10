a6eab813c8cffae91112d1faf7dbd15c5eb1cbdf
Tri VO VAN <vovantri92@gmail.com>
2015-11-10 15:01:38 +0700

    change views for customer manage

--- app/controllers/concerns/admin/employee/users/parameter.rb
+++ app/controllers/concerns/admin/employee/users/parameter.rb
@@ -6,7 +6,7 @@ module Admin::Employee::Users::Parameter
       if params[:user]
         note = params[:user][:note].present? ? params[:user][:note].keys[0..3] : nil
         params.require(:user)
-          .permit(:email, :username, :status, :password, note: note,
+          .permit(:email, :username, :status, :password, :cap, note: note,
             profile_attributes: [:id, :first_name, :last_name, :telephone, :user_id, :avatar],
             address_attributes: [:id, :address_name, :city, :country, :user_id])
       end
