e75b8b6e80c94317c6aee80e2ad0a5fc42492dd1
An Duong <duonghienan.dha@gmail.com>
2015-11-12 10:13:47 +0700

    Change password

--- config/routes/dashboard_routes.rb
+++ config/routes/dashboard_routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :dashboard do
     root to: 'home#index'
     resources :profile, only: [:index, :edit, :update]
+    resources :password, only: [:new, :create]
   end
 end
