7530da2b30e315e0bdedb613949d85e09fcffa28
thienhv <thien.hoviet@gmail.com>
2015-11-12 10:35:16 +0700

    change configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Default mailer url
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => "rubydn.com@gmail.com",
+    :password  => "ZsXaafxYRDAp3QTRWmytbA", # SMTP password is any valid API key
+    :authentication => 'login', # Mandrill supports 'plain' or 'login'
+    # :domain => 'example.com', # your domain to identify your server when connecting
+  }
 end
