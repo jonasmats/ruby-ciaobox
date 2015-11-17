6904cf3abb9210f8112e44b93b98c41480fa23b3
NVTAnh <nvtanh2510@gmail.com>
2015-11-17 14:28:29 +0700

    [feature] Blog publish

--- app/models/user/profile.rb
+++ app/models/user/profile.rb
@@ -1,7 +1,7 @@
 class User::Profile < ActiveRecord::Base
   has_attached_file :avatar,
     styles: { medium: "300x300>", thumb: "100x100>" },
-    default_url: "/images/:style/missing.png",
+    default_url: "missing_avatar.png",
     url: "/images/:class/:attachment/:basename-:hash.:extension",
     hash_secret: "@CiaoboxSecretSocialIcont@" # decode with base64
 
@@ -15,7 +15,7 @@ class User::Profile < ActiveRecord::Base
   end
   # 4 validate
     validates :first_name, :last_name, presence: true, on: :update
-    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/, 
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/,
       presence: true, on: :update
   # 6. instance methods
   def full_name
