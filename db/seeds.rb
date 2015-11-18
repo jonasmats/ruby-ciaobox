ca61dde6f3ab6f17e569d66801b19cda3d4bfed3
thienhv <thien.hoviet@gmail.com>
2015-11-18 14:59:14 +0700

    change item about

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
-# require File.dirname(__FILE__) + '/seeds/faqs'
-# require File.dirname(__FILE__) + '/seeds/driver'
-# require File.dirname(__FILE__) + '/seeds/zip_code'
+require File.dirname(__FILE__) + '/seeds/faqs'
+require File.dirname(__FILE__) + '/seeds/driver'
+require File.dirname(__FILE__) + '/seeds/zip_code'
 
-# require File.dirname(__FILE__) + '/seeds/ciaobox_user/super'
-# require File.dirname(__FILE__) + '/seeds/ciaobox_user/company'
-# require File.dirname(__FILE__) + '/seeds/ciaobox_user/employee'
+require File.dirname(__FILE__) + '/seeds/ciaobox_user/super'
+require File.dirname(__FILE__) + '/seeds/ciaobox_user/company'
+require File.dirname(__FILE__) + '/seeds/ciaobox_user/employee'
 
 
-# require File.dirname(__FILE__) + '/seeds/user'
+require File.dirname(__FILE__) + '/seeds/user'
 
-# require File.dirname(__FILE__) + '/seeds/item/member'
-# require File.dirname(__FILE__) + '/seeds/item/about'
-# require File.dirname(__FILE__) + '/seeds/item/price'
-# require File.dirname(__FILE__) + '/seeds/item/press'
+require File.dirname(__FILE__) + '/seeds/item/member'
+require File.dirname(__FILE__) + '/seeds/item/about'
+require File.dirname(__FILE__) + '/seeds/item/price'
+require File.dirname(__FILE__) + '/seeds/item/press'
 require File.dirname(__FILE__) + '/seeds/item/key_point'
