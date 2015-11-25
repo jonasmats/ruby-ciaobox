dd55f40c7a750308b6503ca1e03c42165dec8c80
softdevelophd <softdevelop.hd@gmail.com>
2015-11-25 10:51:00 +0700

    Update schedule

--- html/frontend/html5/js/custom.js
+++ html/frontend/html5/js/custom.js
@@ -1,4 +1,25 @@
 $=jQuery;
+
+/*function to calculate tatal cost*/
+function cost($quatity, $price) {
+    $TotalCost += $price * $quatity;
+}
+function total_cost() {
+    $TotalCost = 0;
+    $(".store-box-text input").each(function(){
+        $quantity = $(this).val();
+        if ( $.isNumeric($quantity) ) {
+            $store_cost = parseFloat( $(this).parent().parent().parent().find("span .store-cost").text() );
+            cost($quantity, $store_cost);
+        };
+    });
+    /*for orther item*/
+    $TotalCost += 6.25 * ($("#other-item-tags .other-item:last-child()").index() + 1);
+
+    $(".month-total-cost").text($TotalCost);
+}
+/*--End function--*/
+
 $(document).ready(function(){
 
     $(window).scroll(function(){
@@ -33,11 +54,99 @@ $(document).ready(function(){
     });
 */
    
-  
+    /************* Edit shedule.html 24/11/2015 *****************/
 
+    /*--caculate total cost--*/
+    $(".store-box-text input").bind("change", function(){
+        total_cost();
+    });
 
+    /*for checkbox */
+    $(".detail-radio-div").click(function(){
+        $(this).parent().find(".detail-radio-div").removeClass("checked");
+        $(this).addClass("checked");
+    });
+
+    /*when hover store-box*/
+    $(".store-box-text").hover(function(){
+        $(this).addClass("overlay");
+    }, function(){
+        $(this).removeClass("overlay");
+    });
+
+    /*when click store-box*/
+    $(".store-box-normal").click(function(){
+
+        if ( $(this).hasClass("store-box-orther") ) {
+            $(this).addClass("overlay-forever");
+            $("#form-overbottom").slideDown();
+        };
+        $(this).next().addClass("store-box-normal");
+        $(this).next().find("input").val("1");
+
+        if ( $(this).hasClass("store-make-space")) {
+            $(this).next().find("input").val("4");
+            $("#form-overhead").slideDown();
+        };
+
+        $(this).removeClass("store-box-normal");
+
+        /*--caculate total cost--*/
+        total_cost();
+
+    });
+
+    /*when click plus*/
+    $(".js-nav-plus").click(function(){
+        $(this).parent().find("input").val( function(i, oldval) {
+            return ++oldval;
+        });
+        /*--caculate total cost--*/
+        total_cost();
+
+    });
+    /*when click min*/
+    $(".js-nav-min").click(function(){
+        $(this).parent().find("input").val( function(i, oldval) {
+            if( oldval == 1 ) {
+                $(this).parent().parent().parent().find(".store-box-normal").removeClass("store-box-normal");
+                $(this).parent().parent().parent().find(".store-box-first").addClass("store-box-normal");
+                if ( $(this).parent().parent().prev().hasClass("store-make-space") ) 
+                    $("#form-overhead").slideUp();
+            } else return --oldval;
+        });
+        /*--caculate total cost--*/
+        total_cost();
+    });
+
+    /*change value of input*/
+    $(".input-copy-wrapper input").keyup(function(){
+        if ( $(this).val() <= 0 || !$.isNumeric($(this).val()) ) {
+            $(this).parent().parent().parent().find(".store-box-normal").removeClass("store-box-normal");
+            $(this).parent().parent().parent().find(".store-box-first").addClass("store-box-normal");
+            if ( $(this).parent().parent().prev().hasClass("store-make-space") ) 
+                $("#form-overhead").slideUp();
+        };
+    });
+
+    /*for add other item*/
+    $("#input-other-item button").click(function(e){
+        e.preventDefault();
+        $nameItem = $("#input-other-item input").val();
+        if ( !$nameItem.match(/^\s*$/) ) {
+            $("#other-item-tags").append("<div class='other-item'><a class='remove-item'>×</a><span>"+$nameItem+"</span></div>");
+            $("#input-other-item input").val('');
+            /*--caculate total cost--*/
+            total_cost();
+        };
+    }); 
 
 });/*--End---*/
 
-
+/*remove orther item dynamically*/
+$(document).on('click', 'a.remove-item', function(){
+    $(this).parent().remove();
+    /*--caculate total cost--*/
+    total_cost();
+});
 
