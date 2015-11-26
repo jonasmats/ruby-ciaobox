8df7b8c5379493c347b875b0a6a2cdec3d60fd25
An Duong <duonghienan.dha@gmail.com>
2015-11-26 17:00:54 +0700

    finish add base layout

--- app/controllers/shipping/standard_controller.rb
+++ app/controllers/shipping/standard_controller.rb
@@ -5,6 +5,9 @@ class Shipping::StandardController < ApplicationController
   def show
     case step
     when :appoinment
+      @box_order_items = OrderItem::Box.all
+      @normal_order_items = OrderItem::Normal.all
+      
     when :review
       @abc = "123"
     when :confirmation
