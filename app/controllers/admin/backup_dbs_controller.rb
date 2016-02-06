72b07574abd816a4a442c04e755592f8042cfd87
kingcastorim <loosenmind@gmail.com>
2016-02-07 03:03:52 +0700

    Trying fix backup issue

--- app/controllers/admin/backup_dbs_controller.rb
+++ app/controllers/admin/backup_dbs_controller.rb
@@ -38,7 +38,8 @@ class Admin::BackupDbsController < Admin::BaseAdminController
 
     if params[:executable].present? && params[:executable] == '1'
       #cmd = "su - ubuntu -c 'backup perform -t db_backup'"
-      cmd = "backup perform -t db_backup"
+      #cmd = "backup perform -t db_backup"
+      cmd = "sudo -u ubuntu backup perform -t db_backup"
       Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
         #@backup_db[:output] = @backup_db[:output] + "\n" + stdout.read
         strin = stdout.read + " Error => " + stderr.read
